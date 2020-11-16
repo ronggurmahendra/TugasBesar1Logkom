d755f3b4e2c9b4b838ae7ca4f0065efe2616d671
ronggurmahendra <ronggur.mwp@gmail.com>
2020-11-16 15:05:52 +0700

    nambahin player.pl

---
+++ player.pl
@@ -0,0 +1,17 @@
+/*Untuk File Player.pl*/ 
+
+:-dynamic(maxHP/0).
+:-dynamic(currHP/0).
+
+:-dynamic(Defence/0).
+
+:-dynamic(attack/0).
+:-dynamic(specialAttack/0).
+
+:-dynamic(level/0).
+:-dynamic(experience/0).
+
+
+hp(swordman, 200).
+hp(Archer, 150).
+hp(Sorcerer, 100).
